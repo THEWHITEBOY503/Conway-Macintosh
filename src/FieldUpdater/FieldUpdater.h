c61a80146a626018604c993a3fece775a99250f0
Arian Baishya <arian.baishya2004@gmail.com>
2025-05-28 02:17:52 -0500

    fixup! refactor: Field updating into its own class

---
+++ src/FieldUpdater/FieldUpdater.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "BitMapDraw.h"
+#include "Field.h"
+
+class FieldUpdater final {
+private:
+    CellField field;
+    const WindowPtr window;
+    const BitMapDraw bitmapDraw;
+
+    void drawAndUpdate();
+
+public:
+    explicit FieldUpdater(const WindowPtr _window);
+
+    void toggleSingleCell(const uint16_t column, const uint16_t row);
+
+    void nextGeneration();
+
+    void randomResetField();
+
+    void clearField();
+
+    void fillField();
+};
+
+

1f3a71ddce503817a6bb3ea28dbd8b226ea0eaa1
Arian Baishya <arian.baishya2004@gmail.com>
2025-06-01 15:31:20 -0500

    update project building using Docker

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # TODO: only set "universal" env var if user has the files
 # TODO: explain that the dev needs to install docker
 
-docker compose run --rm --build retro68
+docker compose run --rm retro68

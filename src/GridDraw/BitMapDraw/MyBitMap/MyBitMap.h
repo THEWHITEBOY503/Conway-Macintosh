dda785e45931d08f895cb7c3845c91d1f6c541df
Arian Baishya <arian.baishya2004@gmail.com>
2025-06-20 03:24:27 -0500

    edit math for correctness of variable name
    previously, the var held 7 minus the remainder, not just the remainder

--- src/GridDraw/BitMapDraw/MyBitMap/MyBitMap.h
+++ src/GridDraw/BitMapDraw/MyBitMap/MyBitMap.h
@@ -79,13 +79,13 @@ public:
     // TODO: test this with values of x not divisible by 8
     void drawVerticalLine(const uint16_t x, const uint16_t y, const uint16_t length) const {
         const uint16_t x_quotient = x / 8;
-        const uint8_t x_remainder = 7 - (x % 8);
+        const uint8_t x_remainder = x % 8;
 
         for (long i = 0; i < length; ++i) {
             const long row = (y + i) * rowBytes;
             const long offset = row + x_quotient;
 
-            bitmap.baseAddr[offset] |= static_cast<u_char>(1 << x_remainder);
+            bitmap.baseAddr[offset] |= static_cast<u_char>(1 << (7 - x_remainder));
         }
     }
 
